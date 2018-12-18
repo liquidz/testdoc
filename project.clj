fad157ac6bf1c89d24d673cd99cc7dee623a9956
liquidz <liquidz.uo@gmail.com>
2018-12-18 15:00:57 +0900

    ver 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject testdoc "0.1.0-SNAPSHOT"
+(defproject testdoc "0.1.0"
   :description "Yet another doctest implementation in Clojure"
   :url "https://github.com/liquidz/testdoc"
   :license {:name "Eclipse Public License"
