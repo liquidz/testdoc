cec6b6fe9db26730c30bf72ffa8ce9b5c2b8e712
liquidz <liquidz.uo@gmail.com>
2018-12-18 14:55:53 +0900

    Bump clojure to 1.10.0

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/liquidz/testdoc"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :profiles
   {:dev {:dependencies [[fudje "0.9.7"]]}})
