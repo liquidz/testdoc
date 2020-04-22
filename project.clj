15b39c471bcab77f08f60c09fd5d1870a0422395
liquidz <liquidz.uo@gmail.com>
2020-04-22 18:19:14 +0900

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject testdoc "1.1.0"
+(defproject testdoc "1.2.0"
   :description "Yet another doctest implementation in Clojure"
   :url "https://github.com/liquidz/testdoc"
   :license {:name "Eclipse Public License"
